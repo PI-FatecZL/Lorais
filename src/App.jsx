d5419bfc83ed4e69309485d18a240528d52bbc7b
Pedro Odake <pedroodake@gmail.com>
2026-04-18 10:17:59 -0300

    feat: criação do component Hero para diversas telas com props

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import './App.css'
+import './styles/App.css';
 import AppRouters from "./routes/AppRouters.jsx"
 
 function App() {
--- src/routes/AppRouters.jsx
+++ src/routes/AppRouters.jsx
@@ -1,12 +1,14 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 
 import Home from "../pages/Home.jsx"
+import Servicos from "../pages/Servicos.jsx"
 
 function AppRouters() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/servicos" element={<Servicos />} />
       </Routes>
     </BrowserRouter>
   )
